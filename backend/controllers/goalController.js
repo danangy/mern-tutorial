d5bfee5865468940cfd7522b9525fb0b455f4d28
Danang Yanurianto <danangy993@gmail.com>
2022-07-03 21:37:46 +0700

    commit keenam Connecting Controller Functions get, set, update, delete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,35 @@
-
+// @desc    get goals
+// @route   POST /api/goals
+// @access  Private
 const getGoals = (req,res) => {
     res.status(200).json({message: 'Get Goals controller'})
 }
 
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req,res) => {
+    res.status(200).json({message: 'Set Goals controller'})
+}
+
+// @desc    Update goals
+// @route   Put /api/goals/:id
+// @access  Private
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal controller ${req.params.id}`})
+}
+
+// @desc    Delete goals
+// @route   Delete /api/goals/:id
+// @access  Private
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal controller ${req.params.id}`})
+}
 
 module.exports = {
-    getGoals  
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+    
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals } = require('../controllers/goalController')
+const { 
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+} = require('../controllers/goalController')
 
-router.get('/', getGoals)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
