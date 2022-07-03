22b02abc34391ec94d60b82eabe8b541dffd6273
Danang Yanurianto <danangy993@gmail.com>
2022-07-03 17:45:24 +0700

    commit ketiga membuat file route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 console.log("Hello Danang Yanurianto")
-const express = require('express')
+const express = require("express");
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
 const app = express()
 
-app.get('/api/goals',(req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
-})
+app.use('/api/goals',require('./routes/goalRoutes.js'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
