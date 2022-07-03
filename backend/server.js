446d819548f1f3ff0e70da03b8777ebb4e603ddd
Danang Yanurianto <danangy993@gmail.com>
2022-07-03 17:25:02 +0700

    commit kedua membuat route

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: 'Get Goals'})
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
