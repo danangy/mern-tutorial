ecdb229c9316aec60c4d85fa818cf9177570ad12
Danang Yanurianto <danangy993@gmail.com>
2022-07-05 20:30:36 +0700

    Accepting Body Data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,35 @@
-// @desc    get goals
-// @route   POST /api/goals
-// @access  Private
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private 
 const getGoals = (req,res) => {
-    res.status(200).json({message: 'Get Goals controller'})
+    res.status(200).json({message: 'Get Goals'})
 }
-
 // @desc    Set goals
 // @route   POST /api/goals
-// @access  Private
+// @access  Private 
 const setGoal = (req,res) => {
-    res.status(200).json({message: 'Set Goals controller'})
+    if(!req.body.text){
+        res.status(400).json({message: 'Please add a text field'})
+    }
+
+    res.status(200).json({message: 'Set Goal'})
 }
 
 // @desc    Update goals
-// @route   Put /api/goals/:id
-// @access  Private
+// @route   PUT /api/goals/:id
+// @access  Private 
 const updateGoal = (req,res) => {
-    res.status(200).json({message: `Update Goal controller ${req.params.id}`})
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
 }
-
-// @desc    Delete goals
-// @route   Delete /api/goals/:id
-// @access  Private
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private 
 const deleteGoal = (req,res) => {
-    res.status(200).json({message: `Delete Goal controller ${req.params.id}`})
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
 }
-
 module.exports = {
     getGoals,
     setGoal,
     updateGoal,
     deleteGoal
-    
 }
