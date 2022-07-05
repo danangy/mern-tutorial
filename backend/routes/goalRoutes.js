b373f6b2cd4dbff488657bb1b5a4fffabaad0ab7
Danang Yanurianto <danangy993@gmail.com>
2022-07-05 18:27:44 +0700

    menambahkan semua controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,13 +1,25 @@
-const express = require("express");
-const router = express.Router();
-const { 
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal
-} = require('../controllers/goalController')
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.get('/',(req,res)=>{
+    res.status(200).json({message: 'Get Goals'})
+})
+router.get('/', getGoals)
+
+router.post('/',(req,res)=>{
+    res.status(200).json({message: 'Set Goal'})
+})
+router.post('/', setGoal)
+
+router.put('/:id',(req,res)=>{
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+})
+router.put('/:id', updateGoal)
+
+router.delete('/:id',(req,res)=>{
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+})
+router.delete('/:id',deleteGoal)
 
 module.exports = router
