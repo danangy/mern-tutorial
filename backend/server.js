f986522a64e19be436b5147b290a74885416f815
Danang Yanurianto <danangy993@gmail.com>
2022-07-10 18:54:48 +0700

    add mongodb connect

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 console.log("Hello Danang Yanurianto")
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const connectDb = require('./config/db')
+
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+
+connectDb()
 
 const app = express()
 app.use(express.json())
