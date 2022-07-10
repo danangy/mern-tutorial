89fc95381b832e7dcc0a68a0a6ceca13895975e0
Danang Yanurianto <danangy993@gmail.com>
2022-07-10 19:48:21 +0700

    add user route and user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,26 @@
+// @desc    Register user
+// @route   POST /api/users
+// @access  Public
 const registerUser = (req,res) => {
     res.json({ message: "Register User"})
 }
 
-module.exports = { registerUser }
+// @desc    Login user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = (req,res) => {
+    res.json({ message: "Login User"})
+}
+
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Public
+const getMe = (req,res) => {
+    res.json({ message: "User data display"})
+}
+
+module.exports = { 
+    registerUser,
+    loginUser,
+    getMe
+ }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController.js')
+const {
+    registerUser,
+    loginUser,
+    getMe } = require('../controllers/userController.js')
 
 router.post('/',registerUser)
+router.post('/login', loginUser)
+router.get('/me', getMe)
 
 module.exports = router
