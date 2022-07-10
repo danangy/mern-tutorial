32b1ea24bc3f9d68fe04e9b3121079c8bcc9cba1
Danang Yanurianto <danangy993@gmail.com>
2022-07-10 19:37:51 +0700

    test user routes & controller

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
+app.use('/api/users',require('./routes/userRoutes.js'))
 
 app.use(errorHandler)
 
