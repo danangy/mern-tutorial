3b771d99e69948825ee48d414632c635903ebc86
Danang Yanurianto <danangy993@gmail.com>
2022-07-10 19:00:03 +0700

    create a model

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private 
 const getGoals = asyncHandler (async (req,res) => {
-    res.status(200).json({message: 'Get Goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 // @desc    Set goals
 // @route   POST /api/goals
@@ -14,7 +17,11 @@ const setGoal = asyncHandler (async(req,res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set Goal'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 // @desc    Update goals
 // @route   PUT /api/goals/:id
